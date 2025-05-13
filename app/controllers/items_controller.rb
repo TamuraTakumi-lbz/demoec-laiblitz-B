cd9a40c98a8473c8d2d6005384ff2791d6140323
minaxai41211 <minamisawa.kai@laiblitz.co.jp>
2025-05-13 16:41:30 +0900

    devise機能マージ後の動作確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   before_action :authenticate_admin, only: [:dashboard]
-  before_action :authenticate_login_user, only: [:dashboard]
 
   def dashboard
-    @items=Item.includes([:condition,:category]).order(created_at: :desc)
+    @items=Item.order(created_at: :desc)
   end
 
   def index
@@ -12,13 +12,9 @@ class ItemsController < ApplicationController
 
   private
   def authenticate_admin
-    unless current_user.is_admin
+    unless current_user.is_admin?
       redirect_to root_path
     end
   end
 
-  def authenticate_login_user
-     unless user_signed_in?
-      redirect_to root_path
-    end
 end
