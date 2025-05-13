f75be73396bad47f6abf4c8ec9bdc891870bafa8
minaxai41211 <minamisawa.kai@laiblitz.co.jp>
2025-05-13 18:11:41 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_093803) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "categories"
-  add_foreign_key "items", "conditions"
-  
 end
