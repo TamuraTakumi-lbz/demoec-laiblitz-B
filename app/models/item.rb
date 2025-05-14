70a4b0ed29163b91a3be511172c1e124e78b3fd0
imada1290 <imada.yuya@laiblitz.co.jp>
2025-05-14 10:12:41 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+  validates :price, numericality: { greater_than: 300, less_than: 9999999}, format: { with: /\A[0-9]+\z/ }
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
