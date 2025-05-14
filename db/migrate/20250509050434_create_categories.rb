16f8c4b1a309f97ab69908036178e9daee5306b4
TamuraTakumi-lbz <tamura.takumi@laiblitz.co.jp>
2025-05-14 11:08:26 +0900

    モデル構造の修正

--- db/migrate/20250509050434_create_categories.rb
+++ db/migrate/20250509050434_create_categories.rb
@@ -1,13 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.references :purchase, null: false, foreign_key: true, unique: true
-      t.string :postal_code,    null: false                                 
-      t.integer :prefecture_id, null: false                                 
-      t.string :city,           null: false                                 
-      t.string :street_address, null: false
-      t.string :building_name
-      t.string :phone_number,   null: false                                
+      t.text :category
       t.timestamps
     end
   end
