cc93d3c6672eabd739b2507c82fd4bd867893a67
mio-taka <takakuwa.mio@laiblitz.co.jp>
2025-05-14 18:46:44 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,18 +3,27 @@ class ItemsController < ApplicationController
   before_action :authenticate_admin, only: [:dashboard]
 
   def dashboard
-    @items=Item.order(created_at: :desc)
+    @items = Item.order(created_at: :desc)
   end
 
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
-  private
-  def authenticate_admin
-    unless current_user.is_admin?
-      redirect_to root_path
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to items_dashboard_path
+    else
+      render :dashboard
     end
   end
 
+  private
+
+  def authenticate_admin
+    return if current_user.is_admin?
+
+    redirect_to root_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_12_093803) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_13_092008) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,13 +43,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_093803) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.boolean "has_bought"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
-    t.bigint "condition_id", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["condition_id"], name: "index_items_on_condition_id"
+    t.integer "condition_id", null: false
+    t.integer "category_id", null: false
+  end
+
+  create_table "ships", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -73,7 +75,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_093803) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "categories"
-  add_foreign_key "items", "conditions"
-  
 end
