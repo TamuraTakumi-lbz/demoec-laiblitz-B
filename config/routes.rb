9c7c1cbba6317c35617dfb0e0e0c72686184f399
imada1290 <imada.yuya@laiblitz.co.jp>
2025-05-15 16:58:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get  "/admin/sign_up", to: "admin_users#new",    as: :new_admin_registration
     post "/admin",        to: "admin_users#create", as: :admin_registration
+
+    get "admin_users", to: "admin_users#index"
+
   end
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
