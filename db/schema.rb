22c4c0f580f59661761e63424926e5331f49695e
mio-taka <takakuwa.mio@laiblitz.co.jp>
2025-05-15 14:01:52 +0900

    コメントアウトの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_05_13_092008) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_05_14_015505) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,11 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_13_092008) do
     t.datetime "updated_at", null: false
     t.integer "condition_id", null: false
     t.integer "category_id", null: false
-  end
-
-  create_table "ships", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", charset: "utf8mb3", force: :cascade do |t|
@@ -99,7 +92,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_13_092008) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
   add_foreign_key "ships", "purchases", column: "purchases_id"
