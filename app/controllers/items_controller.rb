41d788973d61653f524b2502336bc06a5bdcc356
imada1290 <imada.yuya@laiblitz.co.jp>
2025-05-16 16:06:54 +0900

    テスト修正点

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to items_dashboard_path
     else
       render :new, status: :unprocessable_entity
     end
