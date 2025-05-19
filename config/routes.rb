d15dadd0ec606ee6b0ba2f7a48b4cdfb3050b5c7
minaxai41211 <minamisawa.kai@laiblitz.co.jp>
2025-05-19 11:41:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,13 @@ Rails.application.routes.draw do
 
     get "admin_users", to: "admin_users#index"
     delete "admin_users/:id", to: "admin_users#destroy", as: :admin_user_delete
+    get "/items/dashboard", to: "items#dashboard"
   end
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/items/dashboard", to: "items#dashboard"
+  
   
   # Defines the root path route ("/")
   # root "posts#index"
