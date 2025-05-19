31b0e5ac871a218fa71a543d9fdb3135b70d317a
TamuraTakumi-lbz <tamura.takumi@laiblitz.co.jp>
2025-05-19 15:16:37 +0900

    Update ship.rb

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,8 +1,6 @@
 class Ship < ApplicationRecord
-  attr_accessor :token
-
   validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/,
-                                                    message: 'is invalid. Include hyphen(-)', maximamum: 8 }
+                                                    message: 'is invalid. Include hyphen(-)', maximum: 8 }
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
