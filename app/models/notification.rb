dbc2d5cab8f877679bcd75dafdd225a8c0eca9dd
imada1290 <imada.yuya@laiblitz.co.jp>
2025-05-22 16:47:57 +0900

    バリデーションの実装

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,8 @@
 class Notification < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :starts_at, presence: true
+  validates :starts_at, comparison: {greater_than: Time.now}
+  validates :ends_at, comparison: {greater_than: :starts_at}, allow_nil: true
+  
 end
